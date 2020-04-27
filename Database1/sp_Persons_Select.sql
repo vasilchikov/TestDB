94f3b0edfd514e4c8c75f2b73c05241091678ba4
Васильчиков Дмитрий <vasdm2018@yandex.ru>
2020-04-27 11:16:05 +0300

    #TDP-4 Добавление ХП к таблице Persons
    Создание процедур для работы с таблицей [dbo].[Persons]:
    - [dbo].[sp_Persons_Select] - выборка информации для ФЛ;
    - [dbo].[sp_Persons_Insert] - добавление нового ФЛ;
    - [dbo].[sp_Persons_Update] - обновление ФЛ.

---
+++ Database1/sp_Persons_Select.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_Select]
+	@Id int -- Идентификатор лица
+AS
+BEGIN
+	SELECT [Persons].[Id]
+		  ,[Persons].[Surname]
+		  ,[Persons].[Name]
+		  ,[Persons].[Patronymic]
+		  ,[Persons].[DateOfBirth]
+	FROM   [dbo].[Persons]
+	WHERE  [Persons].[Id]=@Id;
+END
---
+++ Database1/sp_Persons_Update.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_Update]
+	 @Id				int			 --Идентификатор лица
+	,@Surname			nvarchar(50) --Фамилия
+	,@Name				nvarchar(50) --Имя
+	,@Patronymic		nvarchar(50) --Отчество
+	,@DateOfBirth		date		 --Дата рождения
+AS
+BEGIN
+	--SET NOCOUNT ON запрещает вывод колличества строк, на которые влияет
+	--инструкия Transact-SQL, в составе результирующих данных
+	SET NOCOUNT ON;
+
+	UPDATE [dbo].[Persons]
+	   SET [Surname]	=@Surname
+	  ,[Name]			=@Name
+	  ,[Patronymic]		=@Patronymic
+	  ,[DateOfBirth]	=@DateOfBirth
+	WHERE [Id] = @Id;
+END
